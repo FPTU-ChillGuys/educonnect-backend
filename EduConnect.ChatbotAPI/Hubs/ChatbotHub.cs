b49c8bcc0cbfa7bfdf9e5d659217dc936862c978
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-06-28 00:02:02 +0700

    Improve message handling in ChatbotHub.cs
    Refactor the `if` statement for consistency and introduce an asynchronous loop to process responses from `chatbotHelper.ChatbotResponseAsync`. Update caching logic to ensure a new `Message` object is created if `responseMessage` is null, enhancing the clarity and functionality of the message handling process.

--- EduConnect.ChatbotAPI/Hubs/ChatbotHub.cs
+++ EduConnect.ChatbotAPI/Hubs/ChatbotHub.cs
@@ -15,12 +15,12 @@ namespace EduConnect.ChatbotAPI.Hubs
 
         public async Task SendMessageAsync(Guid conversationId, string message)
         {
-           var conversation = await chatbotStorage.GetConversation(conversationId);
+            var conversation = await chatbotStorage.GetConversation(conversationId);
 
             Message newMessage = null;
             Message responseMessage = null;
 
-            if ( conversation != null )
+            if (conversation != null)
             {
                 newMessage = new Message
                 {
@@ -34,10 +34,28 @@ namespace EduConnect.ChatbotAPI.Hubs
                 await chatbotStorage.SaveConversationToCaching(conversationId, newMessage);
                 await conversationService.UpdateConversation(conversation);
 
+                await foreach (var res in chatbotHelper.ChatbotResponseAsync(message, conversationId))
+                {
+                    if (responseMessage == null)
+                    {
+                        responseMessage = new Message
+                        {
+                            Content = res,
+                            ConversationId = conversation.ConversationId,
+                            Role = MessageRole.Assistant.ToString(),
+                            CreatedAt = DateTime.UtcNow
+                        };
+                    }
+                    else
+                    {
+                        responseMessage.Content += res;
+                    }
+                    await Clients.Caller.SendAsync("ReceiveMessage", responseMessage.Content);
+                }
+
+                await chatbotStorage.SaveConversationToCaching(conversationId, responseMessage ?? new Message());
+                await conversationService.UpdateConversation(conversation);
             }
-
         }
-
-
     }
 }
