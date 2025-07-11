f2d74b1de4e891427e76a83be098e293ed5b2ae0
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-07-11 21:01:17 +0700

    Add SubjectPlugin class for subject data retrieval
    Introduces the `SubjectPlugin` class in the `EduConnect.ChatbotAPI.Plugins` namespace. This class implements the `ISubjectService` interface and provides two methods: `GetSubjects` for retrieving a paginated list of subjects, and `GetSubjectByName` for fetching subject details by name. Both methods are annotated for kernel functions, enhancing their integration within a semantic kernel context. Additional using directives for relevant DTOs and services are also included.

---
+++ EduConnect.ChatbotAPI/Plugins/SubjectPlugin.cs
@@ -0,0 +1,47 @@
+﻿using EduConnect.Application.Commons.Dtos;
+using EduConnect.Application.DTOs.Requests.SubjectRequests;
+using EduConnect.Application.DTOs.Responses.SubjectResponses;
+using EduConnect.Application.Interfaces.Services;
+using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace EduConnect.ChatbotAPI.Plugins
+{
+    public class SubjectPlugin (
+            ISubjectService subjectService
+        )
+    {
+        [KernelFunction("GetPagedSubjects")]
+        [Description("Retrieves a paginated list of subjects.")]
+        public async Task<List<SubjectDto>> GetSubjects()
+        {
+            
+            var subjects = await subjectService.GetPagedSubjectsAsync(
+                new SubjectPagingRequest
+                {
+                    PageSize = 100
+                }
+            );
+            return subjects!.Data!;
+
+        }
+
+        [KernelFunction("GetSubjectById")]
+        [Description("Retrieves detailed information about a subject using its ID.")]
+        public async Task<List<SubjectDto>> GetSubjectByName(string subjectName)
+        {
+            var subjects = await subjectService.GetPagedSubjectsAsync(
+                new SubjectPagingRequest
+                {
+                    PageSize = 100,
+                    Keyword = subjectName
+                }
+            );
+            if (subjects.Data == null || subjects.Data.Count == 0)
+            {
+                return new List<SubjectDto>();
+            }
+            return subjects.Data.Where(s => s.SubjectName.Contains(subjectName, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+    }
+}
